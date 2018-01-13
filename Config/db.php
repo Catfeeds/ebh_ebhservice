64b13c6ddbe533b9f154ddff49351cc7ce2739a9
zyp <969742077@qq.com>
2018-01-13 09:49:22 +0800

    还原配置文件

--- Config/db.php
+++ Config/db.php
@@ -8,19 +8,19 @@ return array(
     'dbdriver'  =>  'mysqli',
     'pconnect'  =>  false,
     'dbcharset' =>  'utf8',
-    'dbhost'    =>  '192.168.0.41',
+    'dbhost'    =>  '192.168.0.24',
     'dbuser'    =>  'root',
     'dbport'    =>  3306,
-    'dbpw'      =>  '123456789',
-    'dbname'    =>  'ebh',
+    'dbpw'      =>  '123456',
+    'dbname'    =>  'ebh2',
     'tablepre'  =>  'ebh_',
     'slave'     =>  array(
         array(
-            'dbhost'    =>  '192.168.0.41',
+            'dbhost'    =>  '192.168.0.24',
             'dbuser'    =>  'root',
             'dbport'    =>  3306,
-            'dbpw'      =>  '123456789',
-            'dbname'    =>  'ebh',
+            'dbpw'      =>  '123456',
+            'dbname'    =>  'ebh2',
         )
     )
 );
